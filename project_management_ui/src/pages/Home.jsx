9f46fd478f7fd66b1531c639e455c11eac95c554
Gourav-Shintre <gouravshintre02@gmail.com>
2025-09-28 23:07:01 +0530

    added tailwind css config.

--- project_management_ui/src/pages/Home.jsx
+++ project_management_ui/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1>This is Home page</h1>
+      <h1 className='text-8xl text-red-900 flex'>This is Home page</h1>
     </div>
   )
 }
--- project_management_ui/src/services/baseQuery.js
+++ project_management_ui/src/services/baseQuery.js
@@ -3,6 +3,8 @@ import { fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseQuery = fetchBaseQuery({
   baseUrl: "http://localhost:8080/api/v1",
   prepareHeaders: (headers, { getState }) => {
+    console.log(getState());
+    
     const token = getState().auth?.token || localStorage.getItem("token");
     if (token) {
       headers.set("Authorization", `Bearer ${token}`);
